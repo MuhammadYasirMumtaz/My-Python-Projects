02e11e6aa838cd2846926359b6d4cdc1ea136087
Yasir Mumtaz <ymumtaz676@gmail.com>
2025-07-23 19:23:51 +0500

    Simple Calculator

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 a = int(input("Enter Something:"))
 b = int(input("Enter Something:"))
-if (a>b):
-    print("a is greater")
-elif(a<b):
-    print("b is greater")
-else:
-    print("both are equal")
+Addition = a+b
+Subtraction = a-b
+Multiplication = a*b
+Division = a/b
+print(Addition,Subtraction,Multiplication,Division)
