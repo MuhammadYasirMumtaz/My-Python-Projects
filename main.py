6c2e90bbeebf9a2beaf52ffb315e93f983ae8509
Yasir Mumtaz <ymumtaz676@gmail.com>
2025-07-24 20:20:54 +0500

    Sum Of natural numbers

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-a = int(input("Enter Something:"))
-if (a>=90):
-    print("A")
-elif(a>=80 and a<=89):
-    print("B")
-elif(a>=70 and a<=79):
-    print("C")
-elif(a>=60 and a<=69):
-    print("D")
-elif(a<60):
-    print("F")
-else:
-    print("Absent")
+for i in range (1,11):
+    print(i)
