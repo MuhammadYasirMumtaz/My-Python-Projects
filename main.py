34a5ff6117af1898a569e9cfc213c527a7747fbf
Yasir Mumtaz <ymumtaz676@gmail.com>
2025-07-24 20:51:03 +0500

    Checking Prime And Non Prime Number Using Loop

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 n = int(input("Enter a number: "))
-total = 0
 
-for i in range(1, n + 1):
-    for j in range(i):
-        print("*", end=" ")
-    print()
-    
+if n <= 1:
+    print("Not Prime Number")
+else:
+    for i in range(2, n):
+        if n % i == 0:
+            print("Not Prime Number")
+            break
+    else:
+        print("Prime Number")
+
+
