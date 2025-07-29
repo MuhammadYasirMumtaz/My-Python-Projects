05d29e01ac36c1c865c105b791febaf12c2358a7
Yasir Mumtaz <ymumtaz676@gmail.com>
2025-07-29 16:41:40 +0500

    Factorial Using Recursive Function

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-def average_of_numbers(numbers):
-    return sum(numbers) / len(numbers) 
-
-
-numbers_list = [1, 2, 3, 4, 5]
-result = average_of_numbers(numbers_list)
-print("Average of numbers in the list:", result)
-
-
-
+def fact(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return n*fact(n-1)
+    
+print("The factorial is",fact(5))
 
