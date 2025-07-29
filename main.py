09800aab0e4ee8dfcaba1c2aa455a96caf550479
Yasir Mumtaz <ymumtaz676@gmail.com>
2025-07-29 17:04:33 +0500

    Finding Sum Using Reduce

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from functools import reduce
+
+
 lst = [1,2,3,4,6,7,9,5,0]
 
-Even = list(filter(lambda x : x%2 == 0,lst))
-print("The Even List is:",Even)
+sum = reduce(lambda x, y: x + y, lst)
+print("The Sum is:",sum)
     
 
