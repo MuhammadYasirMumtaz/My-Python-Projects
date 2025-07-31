40ca8ab9bd28228b9b63e09be182139976c169d7
Yasir Mumtaz <ymumtaz676@gmail.com>
2025-07-31 05:05:47 +0500

    Removing Even Numbers From List

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from functools import reduce
+lst = [1, 2, 3, 4, 5, 6]
 
 
-lst = [1,2,3,4,6,7,9,5,0]
-
-sum = reduce(lambda x, y: x + y, lst)
-print("The Sum is:",sum)
-    
-
+for x in lst[:]:
+    if x % 2 == 0:
+        lst.remove(x) 
+        print(f"Removed {x}, current list: {lst}")
+    else:
+        print(f"Odd number {x}, current list: {lst}")
